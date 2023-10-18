8cfdb81a433d57f8831fc0e7cdf27c5dc3358740
Daniel Santana <daniel@everymundo.com>
2023-10-18 11:12:13 -0400

    Complete options and error_handling

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,19 +3,29 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM NOT_DONE
 
 struct Point {
     x: i32,
     y: i32,
 }
 
+fn print_my_string (str: String) {
+    print!("{}", str);
+}
+
 fn main() {
+    let x: f32 = 3.0;
+    let nm = "Poliana";
+    let name = "Steve".to_string();
+
+    print!("name {}", name);
+
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
-        _ => panic!("no match!"),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        _ => {panic!("no match!")},
     }
     y; // Fix without deleting this line.
 }
