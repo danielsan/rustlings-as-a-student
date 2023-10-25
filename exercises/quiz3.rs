daf47c6422355fea85d1355467a31544ebe17ca0
Daniel Santana <daniel@everymundo.com>
2023-10-25 12:19:09 -0400

    Complete quiz3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,15 +16,15 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM NOT_DONE
 
-pub struct ReportCard {
-    pub grade: f32,
-    pub student_name: String,
+pub struct ReportCard<T, TT> {
+    pub grade: T,
+    pub student_name: TT,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: std::fmt::Display, TT: std::fmt::Display> ReportCard<T, TT> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -39,7 +39,7 @@ mod tests {
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
             grade: 2.1,
-            student_name: "Tom Wriggle".to_string(),
+            student_name: "Tom Wriggle",
             student_age: 12,
         };
         assert_eq!(
@@ -52,8 +52,8 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
-            student_name: "Gary Plotter".to_string(),
+            grade: "A+",
+            student_name: "Gary Plotter",
             student_age: 11,
         };
         assert_eq!(
