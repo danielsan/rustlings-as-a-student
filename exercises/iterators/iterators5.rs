e006f99d0e239f0394000d724fed68228294b4ed
Daniel Santana <daniel@everymundo.com>
2023-11-10 16:45:50 -0500

    Complete exercises/iterators

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -11,11 +11,11 @@
 // Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM NOT_DONE
 
 use std::collections::HashMap;
 
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum Progress {
     None,
     Some,
@@ -35,7 +35,7 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    map.values().filter(|val| val == &&value).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -54,7 +54,9 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    collection.iter().fold(0, |acc, map| {
+        acc + count_iterator(map, value)
+    })
 }
 
 #[cfg(test)]
@@ -64,6 +66,7 @@ mod tests {
     #[test]
     fn count_complete() {
         let map = get_map();
+        dbg!(&map);
         assert_eq!(3, count_iterator(&map, Progress::Complete));
     }
 
